6ce8eb5f04a4109e0122deef5546a78cf9aaa97c
chochoprogrammer <michochoprogrammer@gmail.com>
2023-08-05 06:07:56 +0100

    updated with the setup of navbar

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,8 +1,35 @@
 'use client';
+import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
+import { RiFacebookFill, RiWhatsappFill, } from 'react-icons/ri';
 
 
 const Navbar: React.FC = () => {
+  // Nav links Array
+  const navlinks = [
+    {
+      name: 'Home',
+      url: '#',
+      current: false
+    },
+    {
+      name: 'Works',
+      url: '#',
+      current: false
+    },
+    {
+      name: 'About',
+      url: '#',
+      current: false
+    },
+    {
+      name: 'Contact',
+      url: '#',
+      current: false
+    }
+  ]
+
+  // To set the break points
   const [breakpoint, setBreakpoint] = useState(false);
 
   useEffect(() => {
@@ -14,16 +41,45 @@ const Navbar: React.FC = () => {
     return () => {
       window.removeEventListener('resize', handleResize);
     }
-  })
+  }, [])
+
+  // Interface
   return (
-    <header className="bg-surface">
-      <h1 className="text-headerText">Navbar</h1>
-      <a href="" className="link text-linkColor">Link</a>
-      <a href="" className="link text-linkColor">Link</a>
-      <a href="" className="link text-linkColor">Link</a>
-      <a href="" className="link text-linkColor">Link</a>
-      <div className="h-8 w-8 bg-primary"></div>
-      <div className="h-8 w-8 bg-secondary"></div>
+    <header className="bg-surface flex justify-between align-center space-x-3 ">
+      {/* NAV LOGO */}
+      <div className="flex align-center">
+        <Image src='svgs/mi-logo.svg' alt='LOGO' width='30' height='30' />
+        <h2>
+          Emmanuel Michael
+        </h2>
+      </div>
+
+      {/* NAV LINKS */}
+      <nav>
+        <ul>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>Works</a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+        </ul>
+        {/* SOCIALS */}
+        <div>
+          <a href='#'>
+            <RiFacebookFill width='12' height='12' />
+          </a>
+          <a href='#'>
+            <RiWhatsappFill width='12' height='12' />
+          </a>
+          <a href='#'>
+            <RiFacebookFill width='12' height='12' />
+          </a>
+        </div>
+      </nav>
     </header>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Home Page
     </main>
   )
 }
