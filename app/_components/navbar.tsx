8c1f26bb17d90b55ebbdc97082201d27c7e28772
chochoprogrammer <michochoprogrammer@gmail.com>
2023-08-09 15:06:18 +0100

    updated the navar with mobile responsiveness and almost ready

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -22,15 +22,15 @@ const Navbar: React.FC = () => {
       current: false
     },
     {
-      name: 'ABOUT',
+      name: 'BLOG',
       url: '#',
       current: false
     },
     {
-      name: 'BLOG',
+      name: 'CONTACT',
       url: '#',
       current: false
-    }
+    },
   ]
 
   const socialLinks = [
@@ -57,7 +57,12 @@ const Navbar: React.FC = () => {
     const handleScrollDown = () => {
       const currentPosition = window.scrollY;
       const scrollingDown = currentPosition > initialPosition;
-      setScrollDown(!scrollingDown);
+      if (window.innerWidth > 680) {
+        setScrollDown(!scrollingDown);
+      } else {
+        setScrollDown(false);
+      }
+      
       initialPosition = currentPosition;
     };
 
@@ -75,8 +80,12 @@ const Navbar: React.FC = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setBreakpoint(window.innerWidth < 680);
-    }
+      if (window.innerWidth < 680) {
+        setScrollDown(false);
+      } else {
+        setBreakpoint(window.innerWidth < 680);
+      }
+    };
     handleResize();
     window.addEventListener('resize', handleResize);
     return () => {
@@ -86,10 +95,10 @@ const Navbar: React.FC = () => {
 
   // Interface
   return (
-    <header className="bg-transparent fixed top-0 w-full h-[12rem] flex justify-between align-center gap-x-3 px-[5vw] md:px-[10vw]">
+    <header className="bg-transparent fixed top-0 w-full h-[6rem] md:h-[12rem] flex justify-between align-center gap-x-3 px-[5vw] md:px-[10vw]">
       {/* NAV LOGO */}
-      <div className="flex align-center text-left h-full gap-x-[.5rem]">
-        <Image src='svgs/logo-dark.svg' alt='LOGO' width='40' height='40' />
+      <div className="relative z-15 flex align-center text-left h-full gap-x-[.5rem]">
+        <Image src='svgs/logo-dark.svg' alt='LOGO' width='45' height='45' />
         <h2
           className="w-min h-max my-auto text-[.875rem] text-headerText font-lato font-[500] tracking-[.2rem]"
         >
@@ -151,69 +160,124 @@ const Navbar: React.FC = () => {
       {/* NAVBAR BUTTON */}
       <Transition
         as="button"
-        onClick={()=>setScrollDown(true)}
-        className="fixed top-[5rem] right-[5vw] md:right-[calc(10vw)] w-max flex flex-col flex-wrap justify-center gap-y-3"
-        appear={true}
+        onClick={()=>{
+          if (window.innerWidth > 680) {
+            setScrollDown(true);
+          }else {
+            setOpenNav(true);
+          }
+        }}
+        className="fixed top-[2rem] md:top-[5rem] right-[5vw] md:right-[calc(10vw)] w-max flex flex-col flex-wrap justify-center gap-y-3 hover:bg-[#176B8710] rounded-md px-2 py-2"
         show={!scrollDown}
-        unmount={true}
-        enter="transform transition-all opacity ease-in-out delay-[50ms] duration-200"
+        enter="transform transition-all opacity ease-in-out duration-200"
         enterFrom="translate-x-3 opacity-0"
         enterTo="translate-x-0 opacity-1"
         leave="transform transition-all opacity ease-in-out delay-[50ms] duration-75"
         leaveFrom="translate-x-0 opacity-1"
         leaveTo="translate-x-3 opacity-0"
       >
-        <SiCoderwall size="30" color="#176B87" />
+        <SiCoderwall size="30" color="#176B87" className="hover:text-primary" />
       </Transition>
 
 
       {/* NAVBAR FOR SMALL SCREENS */}
       {/* {breakpoint &&  */}
-      <nav
-        className="fixed top-0 right-0 bg-secondary w-[100%] h-[100vh] minH-max flex md:hidden flex-col gap-y-5 align-center text-right px-[calc(1rem+5vw)] pt-[2rem]"
+      <Transition
+        as="nav"
+        show={openNav}
+        className="fixed top-0 right-0 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-purple-600 via-pink-600 to-secondary w-[100%] h-[100vh] minH-max flex md:hidden flex-col gap-y-5 px-[calc(1rem+5vw)] pt-[2rem]"
+        enter="width height transition-all opacity ease-in-out duration-400"
+        enterFrom="w-0 h-0 opacity-0"
+        enterTo="w-full height-full opacity-1"
+        leave="width height transition-all opacity ease-in-out delay-[400ms] duration-400"
+        leaveFrom="w-full height-full opacity-1"
+        leaveTo="w-0 h-0 opacity-0"
       >
         {/* MENUBAR BUTTON */}
-        <button 
-          onClick={()=>setOpenNav(!openNav)}
-          className="px-[.5rem] py-[.5rem] w-max hover:bg-[#ffffff10] rounded-md"
-        >
-          {/* {!openNav && <CgMenuGridO size="30" color="#64CCC5" />} */}
-          <SiPluscodes size="30" color="#64CCC5" className="transform rotate-z-[90deg]" />
-        </button>
+        <div className="w-full flex justify-between">
+          <Transition.Child 
+            className="relative z-15 flex align-center text-left h-full gap-x-[.5rem]"
+            enter="transform transition-all opacity ease-in-out delay-[200ms] duration-200"
+            enterFrom="translate-y-2 opacity-0"
+            enterTo="translate-y-0 opacity-1"
+            leave="transform transition-all opacity ease-in-out delay-[300ms] duration-75"
+            leaveFrom="translate-y-0 opacity-1"
+            leaveTo="translate-y-2 opacity-0"
+          >
+            <Image src='svgs/logo-light.svg' alt='LOGO' width='45' height='45' />
+            <h2
+              className="w-min h-max my-auto text-[.875rem] text-surface font-lato font-[500] tracking-[.2rem]"
+            >
+              EMMANUEL MICHAEL
+            </h2>
+          </Transition.Child>
+          <Transition.Child 
+            onClick={()=>setOpenNav(false)}
+            className="px-[.5rem] py-[.5rem] mr-[-1rem] w-max hover:bg-[#ffffff10] rounded-md"
+            enter="transform transition-all opacity ease-in-out delay-[100ms] duration-200"
+            enterFrom="rotate-0 translate-x-3 opacity-0"
+            enterTo="rotate-45 translate-x-0 opacity-1"
+            leave="transform transition-all opacity ease-in-out delay-[100ms] duration-200"
+            leaveFrom="rotate-45 translate-x-0 opacity-1"
+            leaveTo="rotate-0 translate-x-3 opacity-0"
+          >
+            <SiPluscodes size="30" color="#64CCC5" />
+          </Transition.Child>
+        </div>
 
         {/* NAVBAR LINKS */}
         <ul className="flex flex-col justify-right gap-y-2">
-        {navLinks.map(({name, url}) => {
+        {navLinks.map(({name, url}, index) => {
+          const delayDuration = 50 + (index * 100);
+
           return (
-            <li key={name} className="w-full h-max">
+            <Transition.Child 
+              key={name} 
+              as="li"
+              className="w-full h-max"
+              enter={`transform transition-all opacity ease-in-out delay-[${delayDuration}ms] duration-350`}
+              enterFrom="translate-y-5 opacity-0"
+              enterTo="translate-y-0 opacity-1"
+              leave="transform transition-all opacity ease-in-out delay-[200ms] duration-150"
+              leaveFrom="translate-y-0 opacity-1"
+              leaveTo="translate-y-5 opacity-0"
+            >
               <Link 
                 href={url}
                 className="relative w-full py-[.875rem] px-[calc(1rem+2vw)] flex justify-end text-center align-center text-[1rem] font-open font-[600] text-white rounded-md hover:bg-[#FFFFFF10] hover:pr-[calc(2rem+2vw)] hover:text-primary transition-all duration-200 ease-in-out "
               >
                 {name}
               </Link>
-            </li>
+            </Transition.Child>
           )
         })}
         </ul>
 
         {/* SOCIALS LINKS */}
-        <div className="fixed bottom-[1rem] w-full flex justify-between px-[20vw]">
+        <Transition.Child 
+          className="fixed left-0 bottom-[2rem] w-[100vw] flex justify-evenly"
+          enter={`transform transition-all opacity ease-in-out delay-[250ms] duration-350`}
+          enterFrom="translate-y-3 opacity-0"
+          enterTo="translate-y-0 opacity-1"
+          leave="transform transition-all opacity ease-in-out duration-150"
+          leaveFrom="translate-y-0 opacity-1"
+          leaveTo="translate-y-3 opacity-0"
+        >
           {socialLinks.map(({name, icon}) => {
             return (
               <Link key={name} href='#' aria-label={`socials:${name} link icon`} className="group relative flex justify-center align-center text-center">
-                {React.createElement(icon, {size: 25})}
-                <div className="hidden absolute h-3 w-3 bg-primary top-[calc(100%+.5rem)] rotate-45 group-hover:block z-10 transition-all duration-200 ease-in-out "></div>
+                {React.createElement(icon, {size: 25, color: '#DAFFFB'})}
+                <div className="hidden absolute h-3 w-3 bg-primary bottom-[calc(100%+.5rem)] rotate-45 group-hover:block z-10 transition-all duration-200 ease-in-out "></div>
                 <span 
-                  className="hidden absolute top-[calc(100%+.75rem)] text-[.6rem] text-white bg-primary px-3 py-1 rounded-md group-hover:block z-10 transition-all duration-200 ease-in-out "
+                  className="hidden absolute bottom-[calc(100%+.75rem)] text-[.6rem] text-white bg-primary px-3 py-1 rounded-md group-hover:block z-10 transition-all duration-200 ease-in-out "
                 >
                   {name}
                 </span>
               </Link>
             )
           })}
-        </div>
-      </nav>
+        </Transition.Child>
+      </Transition>
       {/* } */}
     </header>
   )
