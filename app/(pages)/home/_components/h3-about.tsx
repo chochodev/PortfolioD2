717fa765b7c364ce0332d74c22221932fb889d06
chochoprogrammer <michochoprogrammer@gmail.com>
2023-08-13 22:42:18 +0100

    updated real quick to add the first draft of contact template

--- app/(pages)/home/_components/h3-about.tsx
+++ app/(pages)/home/_components/h3-about.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 const AboutSection: React.FC = () => {
   return (
-    <section className="w-full min-h-[100vh] h-max flex flex-col gap-y-[3rem]">
-      <div className="w-full h-max flex justify-evenly items-center text-center flex-wrap gap-x-[.5rem] gap-y-[1.5rem] px-[1rem+5vw]">
+    <section className="w-full min-h-[100vh] flex flex-col gap-y-[2.5rem]">
+      <div className="w-full h-max flex justify-evenly items-center text-center flex-wrap gap-x-[.5rem] gap-y-[2.5rem] px-[1rem+5vw]">
         <Image 
           src="/images/profile02.jpg"
           width={1500}
@@ -16,27 +16,33 @@ const AboutSection: React.FC = () => {
           }}
           className="w-[80%] max-w-[20rem] h-auto border-[.25rem] border-darkSurface/50 shadow-md shadow-darkSuface/50"
         />
-        <div className="w-[40%] min-w-[35rem] h-[20rem] flex text-center justify-center flex-col p-[2vw+1rem] rounded-md border-2 border-primary">
-          <p className="h-max text-[1.05rem] text-textDark spacing-x-[.15rem] leading-[2rem]">
+        <div className="w-[80%] md:w-[40%] md:min-w-[35rem] h-[20rem] flex text-center justify-center flex-col p-[2vw+1rem] rounded-md border-2 border-primary">
+          <p className="w-full h-max text-[1.05rem] text-textDark spacing-x-[.15rem] leading-[2rem]">
             Hi there! I’m Emmanuel Michael, a versatile full-stack web developer with expertise in enhancing and optimizing websites, I specialize in creating modern, user-friendly, and responsive web designs. I possess strong skills in developing web applications from the ground up.
           </p>
         </div>
       </div>
 
       {/* AS A DEVELOPER */}
-      <div className="w-full flex justify-evenly gap-x-[5vw]">
-        <div className="bg-textDark min-w-[20rem] flex flex-col items-center text-center gap-y-[1rem] px-[2vw+1rem] rounded-lg">
+      <div className="w-full flex flex-col md:flex-row justify-evenly gap-x-[5vw]">
+        <div className="bg-textDark min-w-[20rem] flex flex-col items-center text-center gap-y-[1rem] p-[2vw+2rem] rounded-lg">
           <h2 className="text-white text-[1.5rem] font-bold">As a Backend Developer</h2>
           <p className="max-w-[25rem] text-[#cccccc] spacing-x-[.25rem] leading-[1.5rem]">
             I am highly proficient in constructing scalable server systems to empower your websites. Whether it involves refactoring outdated backend code or starting from scratch, I am adept at ensuring robust functionality.
           </p>
         </div>
-        <div className="bg-textDark min-w-[20rem] flex flex-col items-center text-center gap-y-[1rem] px-[2vw+1rem] rounded-lg">
+        <div className="bg-textDark min-w-[20rem] flex flex-col items-center text-center gap-y-[1rem] p-[2vw+2rem] rounded-lg">
           <h2 className="text-white text-[1.5rem] font-bold">As a Frontend Developer</h2>
           <p className="max-w-[25rem] text-[#cccccc] spacing-x-[.25rem] leading-[1.5rem]">
             I am highly skilled in building scalable client-side applications that enhance the functionality of your websites. Whether it&apos;s revamping outdated frontend code or starting from scratch, I ensure robust and seamless user experiences.
           </p>
         </div>
+      </div>
+
+      <div className="w-[80%] md:max-w-[55rem] max-h-[20rem] flex text-center justify-center flex-col p-[2vw+1rem] mx-auto rounded-md border-2 border-primary">
+        <p className="w-full h-max text-[1.05rem] text-textDark spacing-x-[.15rem] leading-[2rem]">
+          Being well-versed in both frontend and backend development, I am equipped to provide valuable assistance for your existing or future web application projects. Count on me to be a valuable asset in bringing your vision to life.
+        </p>
       </div>
     </section>
   )
--- app/(pages)/home/_components/h4-contact.tsx
+++ app/(pages)/home/_components/h4-contact.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { RiPhoneLine, RiUser2Line } from 'react-icons/ri';
 
 const ContactSection: React.FC = () => {
   return (
-    <div>ContactSection</div>
+    <div className="w-full px-[1rem+5vw]">
+      <div className="w-full h-[10rem + 5vw] bg-primary rounded-t-md flex flex-col items-center text-center justify-center gap-[1rem]">
+        <h2 className="text-[2rem] text-headerText bold">Contact me</h2>
+        <p className="texttextLight-">Get in touch with me</p>
+      </div>
+
+      <div className="flex flex-wrap justify-between gap-[.8rem + 5vw]">
+        {/* SOCIAL LINKS */}
+        <div className="max-w-[15rem] w-full flex flex-col items-start gap-y-[1rem]">
+          <div className="flex items-center align-center gap-x-[.5rem]">
+            <RiPhoneLine size="20" color="#64CCC5" />
+            <p className="text-textLight">+234 903 910 8667</p>
+          </div>
+          <div className="flex items-center align-center gap-x-[.5rem]">
+            <RiPhoneLine size="20" color="#64CCC5" />
+            <p className="text-textLight">michochoprogrammer@gmail.com</p>
+          </div>
+          <div className="flex items-center align-center gap-x-[.5rem]">
+            <RiPhoneLine size="20" color="#64CCC5" />
+            <p className="text-textLight">+234 903 910 8667</p>
+          </div>
+        </div>
+
+        {/* CONTACT FORM */}
+        <form action="#" className="w-full flex flex-col p-[1rem] gap-y-[1rem]">
+          <div className="relative flex items-center justify-center">
+            <RiUser2Line size="25" color="#060606" className="absolute left-[.5rem]" />
+            <input type="text" name="name" placeholder="Full name" className="ch-input w-full" />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
