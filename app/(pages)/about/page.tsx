8a34e04d7bc7a8932853391780cb24d9c5cb9d62
chochoprogrammer <michochoprogrammer@gmail.com>
2023-08-17 04:35:13 +0100

    updated for production build

---
+++ app/(pages)/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutSection from '../home/_components/h3-about';
+
+const AboutPage:React.FC = () => {
+  return (
+    <>
+      <AboutSection extra_styles="" />
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ app/(pages)/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactSection from '../home/_components/h4-contact';
+
+const ContactPage = () => {
+  return (
+    <>
+      <ContactSection extra_styles="" />
+    </>
+  );
+}
+
+export default ContactPage;
