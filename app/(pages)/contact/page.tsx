bbacdbe25e6a9643c8981524039eb3e69ecae3de
chochoprogrammer <michochoprogrammer@gmail.com>
2023-08-17 04:55:29 +0100

    twerked for production

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -4,7 +4,7 @@ import ContactSection from '../home/_components/h4-contact';
 const ContactPage = () => {
   return (
     <>
-      <ContactSection extra_styles="" />
+      <ContactSection extra_styles="sm:pt-[7rem] pt-[3rem]" />
     </>
   );
 }
