ea59cb664b057bae62effbdfaa843655559e9099
chochoprogrammer <michochoprogrammer@gmail.com>
2023-08-20 06:17:21 +0100

    updated again with added small float icons

--- app/(pages)/home/_components/h1-home.tsx
+++ app/(pages)/home/_components/h1-home.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import { RiAddCircleFill } from 'react-icons/ri';
+import { RiAddCircleFill, RiLineFill } from 'react-icons/ri';
 
 interface SectionType {
   extra_styles: string
@@ -10,7 +10,7 @@ const HomeSection: React.FC<SectionType> = ({extra_styles}) => {
   return (
     // Add those subtle shapes and all to the background
     <section id="home" className={`flex justify-between items-start mi_md:flex-row flex-col gap-y-[1rem] ${extra_styles}`}>
-      <div className="max-w-[40rem] h-full flex flex-col gap-y-[1.5rem] justify-center px-[calc(4vw+1rem)]">
+      <div className="relative max-w-[40rem] h-full flex flex-col gap-y-[1.5rem] justify-center px-[calc(4vw+1rem)]">
         <h2 className="max-w-[30rem] text-textDark text-[2.5rem] md:text-[3rem] text-bold lg:text-[3.5rem] font-bold">
           I Build, Develop & Design Web Apps.
         </h2>
@@ -35,6 +35,9 @@ const HomeSection: React.FC<SectionType> = ({extra_styles}) => {
           <RiAddCircleFill size="25" color="white" />
           <p className="h-max text-white text-[.875rem] tracking-[.15rem]">Hire me</p>
         </button>
+
+        {/* BACKGROUND ADDED ICONS */}
+
       </div>
       <div 
         className="relative bg-darkSurface z-1 h-full mi_md:w-max w-full flex justify-center items-center py-[calc(3vw+3rem)] mi_md:px-[calc(3vw+2rem)] mi_id-lg:px-[calc(2vw+1.5rem)] border-l-2 border-primary"
@@ -46,7 +49,13 @@ const HomeSection: React.FC<SectionType> = ({extra_styles}) => {
           height={3500}
           className="h-[28rem] w-[calc(15rem+5vw)] object-cover object-right outline outline-[2px] outline-primary outline-offset-[4px] rounded-[10rem]"
         />
-        <div className="absolute z-10 top-[2%] left-[2%] bg-onSurface w-[2rem+1vw] h-[2rem+1vw] rounded-[50%]"></div>
+        <div className="absolute bg-primary/20 z-10 top-[3rem] left-[0] w-[2rem] h-[2rem] rounded-[50%]"></div>
+        <div className="absolute bg-primary/20 z-10 bottom-[5rem] right-[3rem] w-[1.2rem] h-[3.2rem] transform rotate-[55deg]"></div>
+        <div className="absolute bg-primary/20 z-10 top-[1rem] right-[1rem] w-[2rem] h-[2rem] rounded-[50%]"></div>
+        <div className="absolute bg-primary/20 z-10 top-[8rem] left-[5rem] w-[8rem] h-[.15rem] transform rotate-[135deg]"></div>
+        <div className="absolute bg-primary/20 z-10 bottom-[1rem] left-[1rem] rounded-[50%]">
+          <RiLineFill size="15" color="#64ccc5" />
+        </div>
       </div>
     </section>
   )
