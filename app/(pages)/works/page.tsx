ce510234e729c0b6837267c6f99e388291f0b20e
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-04-26 06:27:56 +0100

    resolved the build error

--- app/(pages)/works/page.tsx
+++ app/(pages)/works/page.tsx
@@ -4,7 +4,7 @@ import WorkSection from '../home/_components/h2-works';
 const WorkPage: React.FC = () => {
   return (
     <>
-      <WorkSection extra_styles="sm:pt-[7rem] pt-[4rem]" />
+      <WorkSection extra_styles="bg-white sm:pt-[7rem] pt-[4rem]" />
     </>
   )
 }
